9f609d865d16799b5a28ecc0da6ef54a58776fa2
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-01-20 19:18:55 -0800

    echo msgs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/******* HTTP SERVER USING EXPRESS *******/
+//*****HTTP SERVER USING EXPRESS
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -11,16 +11,18 @@ const
 // Sets server port and logs message on success; server listens for requests in default port or port 1337
 app.set('port', (process.env.PORT || 5000))
 
-//Process data
+//*****Process data
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//Routes
+//*****Routes
 app.get('/', function(req, res) {
   res.send("Hello. Let's talk food!")
 })
 
-//FACEBOOK
+let token = "EAAKyI9NvUzIBABAdgFmHBVaCB4R8YvukhXO7wH51vYUs7kGsyE3vatvfoq0pctVax92R5ZA2cSVzF6RQMF4CMKhD82NZA7gRwGwm43gPpqso4b7WdWkaAo3u2wkBH11qiDufWZApv0YmVtlV2jVk8FP2cW9Pqw4KhEZB14jXxAZDZD"
+
+//*****FACEBOOK
 app.get('/webhook/', function(req, res) {
   if (req.query['hub.verify_token'] == "EAAKyI9NvUzIBABAdgFmHBVaCB4R8YvukhXO7wH51vYUs7kGsyE3vatvfoq0pctVax92R5ZA2cSVzF6RQMF4CMKhD82NZA7gRwGwm43gPpqso4b7WdWkaAo3u2wkBH11qiDufWZApv0YmVtlV2jVk8FP2cW9Pqw4KhEZB14jXxAZDZD"){
       res.send(req.query['hub.challenge'])
@@ -28,6 +30,42 @@ app.get('/webhook/', function(req, res) {
   res.send("Wrong token")
 })
 
+//sends & receive messages from user
+app.post('/webhook/', function(req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for(let i=0; i < messaging_events.length; i++) {
+    let event = messaging_events[i]
+    let sender = event.sender.id
+
+    if(event.message && event.message.text) { //if there's a message & text..
+      let text = event.message.text
+      sendText(sender, "Text echo: " + text.substring(0,100))
+    }
+  }
+  res.sendStatus(200)
+})
+
+//sends message back to user
+function sendText(sender, text){
+  let messageData = {text: text}
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token : token},
+    method: "POST",
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function(error, response, body) {
+    if(error) {
+      console.log("sending error")
+    } else if(response.body.error) {
+      console.log("response body error")
+    }
+  })
+}
+
+//**PORT
 app.listen(app.get('port'), function() {
   console.log("running: port")
 })
