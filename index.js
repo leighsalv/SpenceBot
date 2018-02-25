0a76167993cd631d1a58de6fd36bd77cc37a2113
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-02-25 01:24:23 -0800

    verified money, added jokes

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json())
 
 //Routes
 app.get('/', function(req, res) {
-  res.send("Hello. Let's talk food!")
+  res.send("Hello! I'll help you keep track of your expenses!")
 })
 
-let token = "EAAKyI9NvUzIBAIwUJHZCyMKUnz9CeaOnWypiOXgSO9EnvFLtZAmpBRuY7w07T3XtZB0Tk3dGJeTUjVOIkQGJflwS8P8ZAHIPB9gzbUrj5VTia57SsAZB83YXfgROEkjxUiiHEdbpSXhaeqwaDDvbS66emucwyN9n6jQiwvVjpLwZDZD"
+let token = ""
 
 
 
@@ -28,7 +28,7 @@ let token = "EAAKyI9NvUzIBAIwUJHZCyMKUnz9CeaOnWypiOXgSO9EnvFLtZAmpBRuY7w07T3XtZB
 
 // ===== FACEBOOK ===============================================================
 app.get('/webhook/', function(req, res) {
-  if (req.query['hub.verify_token'] == "EAAKyI9NvUzIBAIwUJHZCyMKUnz9CeaOnWypiOXgSO9EnvFLtZAmpBRuY7w07T3XtZB0Tk3dGJeTUjVOIkQGJflwS8P8ZAHIPB9gzbUrj5VTia57SsAZB83YXfgROEkjxUiiHEdbpSXhaeqwaDDvbS66emucwyN9n6jQiwvVjpLwZDZD"){
+  if (req.query['hub.verify_token'] == ""){
       res.send(req.query['hub.challenge'])
   }
   res.send("Wrong token")
