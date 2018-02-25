3a656391f23deae4cd42c1a064c1bb5f29425e11
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-02-24 17:05:23 -0800

    or operator

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ app.post('/webhook/', function(req, res) {
     let sender = event.sender.id
 
     if(event.message && event.message.text) { //if there's a message & text..
-      var text = event.message.text
+      let text = event.message.text
 
-      if(text == 'help' || 'HELP') { //user typed help -> bot tells instructions
+      if(text == "help" || text == "HELP" || text == "Help") { //user typed help -> bot tells instructions
         sendText(sender, "Type...\nTOTAL to see how much you've spent\nCLEAR to restart log\n\nTo log money, type the amount i.e. 10 or 10.00 so I can keep track of how much you spend!")
         continue
       }
@@ -55,7 +55,7 @@ app.post('/webhook/', function(req, res) {
       	continue
       }
 
-      else if(text == 'total' || 'TOTAL') { //user typed total -> bot adds & replies total amount spent
+      else if(text == "total" || text == "TOTAL" || text == "Total") { //user typed total -> bot adds & replies total amount spent
       	totalAmount = 0;
       	for(var j = 0; j < spent.length; j++) {
       		totalAmount = totalAmount + spent[j];
@@ -64,7 +64,7 @@ app.post('/webhook/', function(req, res) {
       	continue
       }
 
-      else if(text == 'clear' || 'CLEAR') { //user typed clear -> bot clears spent log (array)
+      else if(text == "clear" || text == "CLEAR" || text == "Clear") { //user typed clear -> bot clears spent log (array)
       	spent.length = 0;
       	sendText(sender, "Log cleared")
       	continue
